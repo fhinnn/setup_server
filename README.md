012c5189443b3e4b9b4141482db309a2eb6a7032
fhinnn <deltras12388@gmail.com>
2024-05-17 02:11:22 +0700

    feat add setup.sh

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # setup_server
+
+a. Timezone Asia/Jakarta
+b. update & upgrade
+b. Install Git, Curl , ZIP , python3 & python3-pip
+d. Install docker
---
+++ setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+sudo apt-get update && sudo apt-get upgrade -y
+sudo timedatectl set-timezone Asia/Jakarta
+sudo apt-get install -y git curl zip python3 python3-pip
+sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update -y
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo usermod -aG docker $USER
+
+echo "SELESAI !!!"
